4f1683c5e7e743f402c88daa1bbf5e870ffaeaab
Edward Leung <edward_leung_12@hotmail.com>
2020-09-18 17:37:54 +0100

    Now we have a functional component for navbar and user item.

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const UserItem = props => {
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
     <div className='card text-center'>
       <img
-        src={props.user.avatar_url}
+        src={avatar_url}
         alt=''
         className='round-img'
         style={{ width: '60px' }}
       />
-      <h3>{props.user.login}</h3>
+      <h3>{login}</h3>
 
       <div>
-        <a href={props.user.html_url} className='btn btn-dark btn-sm my-1'>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
           More
         </a>
       </div>
